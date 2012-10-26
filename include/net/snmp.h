8dbbdeafd2f7ccaada9c10adad1aa86113af05b1
Christoph Paasch <christoph.paasch@uclouvain.be>
2012-10-26 14:34:38 +0200

    Fix sysctl
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- include/net/snmp.h
+++ include/net/snmp.h
@@ -154,8 +154,12 @@ struct linux_xfrm_mib {
 #define SNMP_ADD_STATS(mib, field, addend)	\
 			irqsafe_cpu_add(mib[0]->mibs[field], addend)
 
-#define SNMP_RESET_STATS(mib, field)	\
-			__this_cpu_write(mib[0]->mibs[field], 0)
+#define SNMP_RESET_STATS(mib, field)					\
+		do {							\
+			local_bh_disable();				\
+			__this_cpu_write(mib[0]->mibs[field], 0)	\
+			local_bh_enable();				\
+		} while (0)
 /*
  * Use "__typeof__(*mib[0]) *ptr" instead of "__typeof__(mib[0]) ptr"
  * to make @ptr a non-percpu pointer.
